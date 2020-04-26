eddc293e1dfd8fd7ca1d1911a4e81cfc2f0ff7fb
Fitz Ryland <fitzryland@gmail.com>
2020-04-26 08:36:01 -0700

    moved session actions to its own component. made some notes on next steps

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Fitzy's Tic Tac Toe #
+
+* who's turn is it
+* who is playing the current game
+* which player is X and O
+* cap on how many players can be a part of the game
+
+* is each checked square an X or O
+* has anyone won yet?
--- tic-tac-toe-client/src/components/TicTacToeBoard.vue
+++ tic-tac-toe-client/src/components/TicTacToeBoard.vue
@@ -2,35 +2,7 @@
   <div class="tic_tac_toe">
     <header class="header">
       <User />
-      <div class="session_actions">
-        <button
-          class="
-            session_actions__icon
-            session_actions__icon--share
-          "
-          v-on:click=toggleShare
-        >
-          <ShareIcon />
-        </button>
-        <button
-          class="session_actions__icon"
-          v-on:click=resetGame
-        >
-          <RestartIcon />
-        </button>
-        <button
-          class="session_actions__icon"
-          v-on:click=leaveGame
-        >
-          <ExitIcon />
-        </button>
-        <div
-          class="share"
-          v-bind:class="{'is-closed': hideShare}"
-        >
-          Game ID: {{ gameId }}
-        </div>
-      </div>
+      <SessionActions />
     </header>
     <div class="board">
       <button
@@ -48,16 +20,12 @@
 <script>
   import VueSocketIO from 'vue-socket.io'
   import User from './User.vue'
-  import RestartIcon from '../assets/icons/refresh-cw.svg'
-  import ExitIcon from '../assets/icons/x.svg'
-  import ShareIcon from '../assets/icons/share.svg'
+  import SessionActions from './SessionActions.vue'
   export default {
     name: 'TicTacToeBoard',
     components: {
       User,
-      RestartIcon,
-      ExitIcon,
-      ShareIcon
+      SessionActions
     },
     data() {
       return {
@@ -124,19 +92,8 @@
         }
         await this.$socket.emit('clickBox', message)
       },
-      async resetGame() {
-        await this.$socket.emit('resetGame', this.$store.state.gameId)
-      },
-      async leaveGame() {
-        await this.$socket.emit('leaveGame', this.$store.state.gameId, this.$store.state.userId)
-        this.$store.commit('setGameId', null)
-        this.$router.push('/')
-      },
       async registerUser() {
         await this.$socket.emit('registerUser', this.$store.state.userId)
-      },
-      toggleShare() {
-        this.hideShare = ! this.hideShare
       }
     }
   }
@@ -173,52 +130,5 @@
   display: flex;
   justify-content: space-between;
   padding: 10px 20px;
-}
-.session_actions {
-  display: flex;
-  position: relative;
-  &__icon {
-    align-items: center;
-    background-color: $c-teal;
-    border: none;
-    border-radius: 100%;
-    display: flex;
-    height: 35px;
-    justify-content: center;
-    margin-left: 15px;
-    overflow: hidden;
-    padding: 0;
-    width: 35px;
-    &--share {}
-  }
-}
-.share {
-  background-color: $c-darkPurple;
-  border: 2px solid $c-teal;
-  padding: 15px 15px 20px 15px;
-  left: -4px;
-  position: absolute;
-  top: calc(100% + 18px);
-  transition: opacity 0.5s ease, transform 0.7s ease;
-  &.is-closed {
-    opacity: 0;
-    pointer-events: none;
-    transform: translateY(-20px);
-  }
-  &:before {
-    background-color: $c-darkPurple;
-    border-top: 2px solid $c-teal;
-    border-right: 2px solid transparent;
-    border-bottom: 2px solid transparent;
-    border-left: 2px solid $c-teal;
-    content: "";
-    display: block;
-    height: 18px;
-    left: 20px;
-    position: absolute;
-    transform: rotate(45deg);
-    top: -11px;
-    width: 18px;
-  }
 }
 </style>
